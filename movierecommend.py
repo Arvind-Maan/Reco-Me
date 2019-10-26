dc1c56de9c57cf0d90e9dabfc9bd2cba9761729d
Arvind Maan <arvindmaan1199@gmail.com>
2019-10-26 01:04:07 -0500

    Added MovieLens Dataset

--- movierecommend.py
+++ movierecommend.py
@@ -3,3 +3,26 @@
 # 1. how we can improve recommendation
 # 2. how a program like this could run
 # 3. get a better interpretation of the algorithm
+
+import pandas as pd
+# pass in column names for each CSV
+u_cols = ['user_id', 'age', 'sex', 'occupation', 'zip_code']
+users = pd.read_csv('ml-100k/u.user', sep='|', names=u_cols,
+                    encoding='latin-1')
+
+r_cols = ['user_id', 'movie_id', 'rating', 'unix_timestamp']
+ratings = pd.read_csv('ml-100k/u.data', sep='\t', names=r_cols,
+                      encoding='latin-1')
+
+# the movies file contains columns indicating the movie's genres
+# let's only load the first five columns of the file with usecols
+m_cols = ['movie_id', 'title', 'release_date', 'video_release_date', 'imdb_url']
+movies = pd.read_csv('ml-100k/u.item', sep='|', names=m_cols, usecols=range(5),
+                     encoding='latin-1')
+
+# create one merged DataFrame
+movie_ratings = pd.merge(movies, ratings)
+lens = pd.merge(movie_ratings, users)
+most_rated = lens.groupby('title').size().sort_values(ascending=False)[:25]
+most_rated
+print(most_rated)
