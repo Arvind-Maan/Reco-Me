bcd0025a1e08c21a6e20eed4a5120ec264330f86
Arvind-Maan <arvindmaan1199@gmail.com>
2019-10-25 23:17:19 -0500

    Initial Commit

---
+++ movierecommend.py
@@ -0,0 +1,5 @@
+#this is a prototype to see how Collabrative Filtering could work in the context with movies
+#From this prototype, we want to find out,
+# 1. how we can improve recommendation
+# 2. how a program like this could run
+# 3. get a better interpretation of the algorithm
