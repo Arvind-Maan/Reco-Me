146b7cb0289bb4ef2042218f8d624c33ed2380e9
Arvind Maan <arvindmaan1199@gmail.com>
2019-12-05 22:39:44 -0600

    made user friendly prompts

--- movie_recommendation.py
+++ movie_recommendation.py
@@ -7,6 +7,7 @@
 import pandas as pd
 import numpy as np
 import math
+import random
 
 # user data 
 # take the first 4 columns cause the 5th column is postal code, which we don't really care about.
@@ -30,6 +31,50 @@ movie_ratings = pd.merge(movies, ratings)
 movie_features = pd.merge(movie_ratings, users).pivot(index='user_id',columns='movie_id',values='rating').fillna(0)
 mu_matrix = np.array(movie_features.values, dtype=int)
 
+
+"""
+add_user
+add yourself to the matrix
+"""
+def add_user():
+    print("\n\nIn order for us to accurately recommend you a movie, we need to get an understanding of some of the movies you like.")
+    print("You must rank at least 10 movies so we can get an understanding of your taste :)")
+
+    # we need to create an array of rankings
+    user_row = [0 for i in range(0, len(mu_matrix[0]))]
+    num = 0 # number of current movies ranked
+    while num < 10:
+        # pick a random movie and make the user rank it. WE NEED 10 rankings.
+        movie_index = random.randint(0,len(mu_matrix[0])-1)
+        if(user_row[movie_index] <= 0):
+            movie_x = (movies.fillna(-1)).loc[movie_index] #get the movie summary
+            print("~~~What do you think of this movie?~~~")
+            #print the movie
+            to_string = "Movie \n\t"
+            for val in movie_x.values:
+                if val != -1: # ignore nan values
+                    to_string += str(val) + " | " 
+            print(to_string)
+            #if the users answer is valid
+            valid = False
+            while not valid: #until we get a valid answer, we cannot continue
+                x = input("Please enter a rating 1-5, 0 if no opinion: ")
+                print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+                if( x.isdigit() and int(x) >= 0 and int(x) <= 5):
+                    user_row[movie_index] = int(x)
+                    num += 1 if int(x) != 0 else 0 # if its 0, we don't increment the ratings we've gotten
+                    valid = True
+                else: 
+                    print("~Invalid Input~")
+                    valid = False
+    # we have to return the updated array
+    # since if we try to change mu_matrix in this method we will get an error.
+    # not this does not add the user to our user database or rating database. this is a bandaid fix and only stores the user for runtime
+    convert_row = [[item for item in user_row]]
+    copy_matrix = np.append(mu_matrix, convert_row, axis = 0)
+    return copy_matrix
+    
+
 """
 Euclidean Distance
 Determines the "Distance" or Simularity between 2 users by checking each of their common rankings and what they ranked them
@@ -43,7 +88,7 @@ def euclidean_distance(user1,user2):
     # so each user is their index in the matrix :d
     # find all the mutual rankings and store the difference between those 2 rankings ^2
     common_rankings = [(mu_matrix[user1][i] - mu_matrix[user2][i])**2 for i in range(0,len(mu_matrix[user1])-1) if mu_matrix[user1][i] > 0 and mu_matrix[user2][i] > 0]
-    return math.sqrt(sum(common_rankings))
+    return 1 / (1 + sum(common_rankings))
 
 """
 Pearson Simularity
@@ -84,6 +129,7 @@ def cosine_similarity(user1, user2):
     # get denominator so we can check division by 0
     denom = (math.sqrt(sum_a_sqr)*math.sqrt(sum_b_sqr))
     return (sum_ab / denom) if denom != 0 else 0
+
 """
 Square
 returns the list with every single value square'd
@@ -106,44 +152,43 @@ def recommend (user, clusters, fn, k):
     print("Using %s for calculating similarity/distance with %d clusters" %(fn,clusters))
     # we want to use our simularity functions to develop a list of ratings that our user might rate a movie as.
     # this allows us to think what our user will like the most in comparison
-    # get the distances 
-    distances = [(fn(user, other),other) for other in range(0,len(mu_matrix)-1) if other != user]
-    u_items = mu_matrix[user]
-    # sort and reverse the data (closest -> least closest) and attach a bound 
-    distances.sort() 
-    distances.reverse()
-    distances = distances[0:clusters] 
+    distances = simularity_matrix(user,fn,clusters)
     #for each distance 
-    recommendations = {}
-    for dist,o in distances:
-        o_items = mu_matrix[o]
+    all_unseen_movies = {}
+    for dist,o in distances: 
         # each movie is an index, the number of movies is the LENGTH of the items row
-        for i in range(0, len(o_items)-1):
-            # if we haven't ranked this item yet, we must rank it.
-            if(o_items[i] > 0 and u_items[i] <= 0):
-                # unfortunately it's tough to stick to our 1 -> 5 ranking here for obvious reasons
-                predicted_rank = int(dist*o_items[i]) 
-                # if we are already recommending this movie, push it higher!
-                if(i not in recommendations):
-                    # add it to recommendations
-                    recommendations[i] = (dist, [predicted_rank])
-                else:
-                    # update the values
-                    sim, p_rank = recommendations[i]
-                    recommendations[i] = (sim + dist, p_rank + [predicted_rank])
-    # for each recommendation         
-    for x in recommendations:
-        dist,rankings = recommendations[x]
-        recommendations[x] = sum(rankings)/dist if dist != 0 else 0
+        for i in range(0, len(mu_matrix[o])-1):
+            predicted_rank = int(dist*mu_matrix[o][i])
+            # update recommendations i to be the value of the similarity predicted rank 
+            if (mu_matrix[o][i] > 0 and mu_matrix[user][i] <= 0): #if they ranked it but we didn't
+                all_unseen_movies[i] = (dist, [predicted_rank]) if i not in all_unseen_movies else (all_unseen_movies[i][0] + dist, all_unseen_movies[i][1] + [predicted_rank])   
+    # update all the values
+    for m in all_unseen_movies: 
+        all_unseen_movies[m] = sum(all_unseen_movies[m][1])/all_unseen_movies[m][0] if all_unseen_movies[m][0] != 0 else 0 
     # at this point we have a dictionary of all possible recommendations with the format:
     # { [movie_id] : [predicted_rating]}
     # lets pick the top 5!
-    recommend_movies = filter_results(recommendations,k)
+    recommend_movies = filter_results(all_unseen_movies,k)
     # recommend movies outputs an array formatted like so: [ (movie_id, predicted rank)]
     # lets print it in a nice way:
     print_recommendations(recommend_movies)
     print("############ END OF RECOMMENDATION FOR USER %d ############" %(user))
     return recommend_movies
+
+"""
+simularity matrix
+    returns a matrix of users that are similar to our user and how similar they are.
+    [ (user similarity, user), (user similarity, user), .... , ]
+    usings [fn] to calculate simularity
+    and limits the matrix to size clusters
+"""
+def simularity_matrix(user, fn, clusters):
+    # get the distances 
+    # sort and reverse the data (closest -> least closest) and attach a bound 
+    toReturn = [(fn(user, other),other) for other in range(0,len(mu_matrix)-1) if other != user]
+    toReturn.sort(reverse=True)
+    toReturn = toReturn[0:clusters] 
+    return toReturn
 
 import operator,collections
 
@@ -217,10 +262,24 @@ def print_user_summary(user):
 
 
 # sample entries 
-user = 1
-bound = 10
+print("\n\n____________________Welcome to Project: Reco-ME____________________")
+print("You have the option between recommending a movie to yourself or to an existing user in the database")
+answer = input("Would you like to recommend to yourself? Please enter (Y/N): ")
+if(answer == "Y" or answer == "y"):
+    mu_matrix = add_user()
+    user = len(mu_matrix)-1
+else:
+    user = int(input("Please enter the ID of the existing user you'd like to recommend to [Note: Max is 943](Y/N): "))
+
+print("Please enter the number of clusters you'd like to test with: (a good one to try is 10)")
+bound = int(input("[Note: Max is 943]"))
+
 k = 5
-print_user_summary(user)
+
+print("Recommendations with Euclidean Distance")
 recommend(user, bound, euclidean_distance,k)
+print("\nRecommendations with Pearson Similarity")
 recommend(user, bound, pearson_similarity,k)
+print("\nRecommendations with Cosine Similarity")
 recommend(user, bound, cosine_similarity,k)
+print("\n\n____________________END OF SIMULATION____________________")
